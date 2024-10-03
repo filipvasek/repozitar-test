97449f7c6467c7077dbffbc1f7cfbeb5c7cc5732
Student <Student@PC-SE5113-20>
2024-10-03 14:29:14 +0200

    vektorz

---
+++ Cv03/vektory/vektory/vektory.cpp
@@ -0,0 +1,21 @@
+﻿// vektory.cpp : Defines the entry point for the application.
+//
+
+#include "vektory.h"
+
+using namespace std;
+
+int main()
+{
+	int vektorA;
+	int vektorB;
+
+	printf("Zadejte vektor A a vektor B: ");
+	scanf_s("%d %d", &vektorA, &vektorB);
+
+	int soucet;
+	soucet = vektorA + vektorB;
+
+	printf("Soucet vektoru A a vektoru B = %d\n", soucet);
+
+}
