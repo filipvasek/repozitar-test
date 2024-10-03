651ea793c172528a486e1def06f2c82d4790621e
Student <Student@PC-SE5113-20>
2024-10-03 14:17:09 +0200

    bourka

---
+++ Cv03/bourka/CMakeProject1/CMakeProject1.cpp
@@ -0,0 +1,23 @@
+﻿// CMakeProject1.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeProject1.h"
+
+using namespace std;
+
+int main()
+{
+	int cas;
+	
+	printf("Zadejte cas za ktery se ozve hrom: ");
+	scanf_s("%d", &cas);
+
+	int vzdalenost;
+
+	vzdalenost = cas * 340;
+
+
+	printf("Vzdalenost mista je = %d\n", vzdalenost);
+
+	return 0;
+}
